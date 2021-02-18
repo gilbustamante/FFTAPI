4102d356f698799e92350450dc57876d0feb1afd
Gil Bustamante <65842842+gilbustamante@users.noreply.github.com>
2021-02-18 00:02:58 -0800

    Both item endpoints are working correctly

--- controllers/api.js
+++ controllers/api.js
@@ -1,7 +1,16 @@
-module.exports.allEndpoints = (req, res) => {
+const Item = require('../models/item');
+
+module.exports.allEndpoints = async (req, res) => {
   res.send('JSON response will be here');
 }
 
-module.exports.allItems = (req, res) => {
-  
+module.exports.allItems = async (req, res) => {
+  const items = await Item.find({});
+  res.send(items);
+}
+
+module.exports.oneItem = async (req, res) => {
+  const query = req.params.query;
+  const item = await Item.findOne({ nameLower: query.toLowerCase() });
+  res.send(item);
 }
--- routes/api.js
+++ routes/api.js
@@ -5,4 +5,10 @@ const router  = express.Router();
 router.route('/')
   .get(api.allEndpoints)
 
+router.route('/item')
+  .get(api.allItems)
+
+router.route('/item/:query')
+  .get(api.oneItem)
+
 module.exports = router;
