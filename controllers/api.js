8cc83c969898c8cf9f70c343a884f772cc27c75a
Gil Bustamante <65842842+gilbustamante@users.noreply.github.com>
2021-02-23 20:58:21 -0800

    Add random route/controller for each category

--- controllers/api.js
+++ controllers/api.js
@@ -1,10 +1,11 @@
-const Accessory = require('../models/accessory');
 const mongoose  = require('mongoose');
+const Accessory = require('../models/accessory');
 const Armor     = require('../models/armor');
 const Helmet    = require('../models/helmet');
 const Item      = require('../models/item');
 const Weapon    = require('../models/weapon');
 
+// All endpoint categories
 module.exports.allEndpoints = async (req, res) => {
   mongoose.connection.db.listCollections().toArray((err, nameObjects) => {
     if (err) {
@@ -18,57 +19,107 @@ module.exports.allEndpoints = async (req, res) => {
   })
 }
 
-module.exports.allItems = async (req, res) => {
-  const items = await Item.find({});
-  res.send(items);
+// Accessories - All
+module.exports.allAccessories = async (req, res) => {
+  const accessories = await Accessory.find({});
+  res.send(accessories);
 }
 
-module.exports.oneItem = async (req, res) => {
+// Accessories - Random
+module.exports.randomAccessory = async (req, res) => {
+  const accessoryCount = await Accessory.count();
+  const randNum = Math.floor((Math.random() * accessoryCount) + 1);
+  const weapon = await Accessory.findOne().skip(randNum);
+  res.send(weapon)
+}
+
+// Accessories - One
+module.exports.oneAccessory = async (req, res) => {
   const query = req.params.query;
-  const item = await Item.findOne({ nameLower: query.toLowerCase() });
-  res.send(item);
+  const accessory = await Accessory.findOne({ nameLower: query.toLowerCase() });
+  res.send(accessory);
 }
 
+// Armor - All
 module.exports.allArmor = async (req, res) => {
   const armor = await Armor.find({});
   res.send(armor);
 }
 
+// Armor - Random
+module.exports.randomArmor = async (req, res) => {
+  const armorCount = await Armor.count();
+  const randNum = Math.floor((Math.random() * armorCount) + 1);
+  const armor = await Armor.findOne().skip(randNum);
+  res.send(armor)
+}
+
+// Armor - One
 module.exports.oneArmor = async (req, res) => {
   const query = req.params.query;
   const armor = await Armor.findOne({ nameLower: query.toLowerCase() });
   res.send(armor)
 }
 
+// Helmets - All
 module.exports.allHelmets = async (req, res) => {
   const helmets = await Helmet.find({});
   res.send(helmets);
 }
 
+// Helmets - Random
+module.exports.randomHelmet = async (req, res) => {
+  const helmetCount = await Helmet.count();
+  const randNum = Math.floor((Math.random() * helmetCount) + 1);
+  const helmet = await Helmet.findOne().skip(randNum);
+  res.send(helmet)
+}
+
+// Helmets - One
 module.exports.oneHelmet = async (req, res) => {
   const query = req.params.query;
   const helmet = await Helmet.findOne({ nameLower: query.toLowerCase() });
   res.send(helmet)
 }
 
+// Items - All
+module.exports.allItems = async (req, res) => {
+  const items = await Item.find({});
+  res.send(items);
+}
+
+// Items - Random
+module.exports.randomItem = async (req, res) => {
+  const itemCount = await Item.count();
+  const randNum = Math.floor((Math.random() * itemCount) + 1);
+  const item = await Item.findOne().skip(randNum);
+  res.send(item)
+}
+
+// Items - One
+module.exports.oneItem = async (req, res) => {
+  const query = req.params.query;
+  const item = await Item.findOne({ nameLower: query.toLowerCase() });
+  res.send(item);
+}
+
+// Weapons - All
 module.exports.allWeapons = async (req, res) => {
   const weapons = await Weapon.find({});
   res.send(weapons);
 }
 
+// Weapons - Random
+module.exports.randomWeapon = async (req, res) => {
+  const weaponCount = await Weapon.count();
+  const randNum = Math.floor((Math.random() * weaponCount) + 1);
+  const weapon = await Weapon.findOne().skip(randNum);
+  res.send(weapon)
+}
+
+// Weapons - One
 module.exports.oneWeapon = async (req, res) => {
   const query = req.params.query;
   const weapon = await Weapon.findOne({ nameLower: query.toLowerCase() });
   res.send(weapon);
-}
-
-module.exports.allAccessories = async (req, res) => {
-  const accessories = await Accessory.find({});
-  res.send(accessories);
-}
-
-module.exports.oneAccessory = async (req, res) => {
-  const query = req.params.query;
-  const accessory = await Accessory.findOne({ nameLower: query.toLowerCase() });
-  res.send(accessory);
 }
--- routes/api.js
+++ routes/api.js
@@ -3,37 +3,48 @@ const api        = require('../controllers/api');
 const catchAsync = require('../utils/catchAsync');
 const router     = express.Router();
 
+// Root
 router.route('/')
   .get(catchAsync(api.allEndpoints))
 
-router.route('/items')
-  .get(catchAsync(api.allItems))
+// Accessories
+router.route('/accessories')
+  .get(catchAsync(api.allAccessories))
+router.route('/accessories/random')
+  .get(catchAsync(api.randomAccessory))
+router.route('/accessories/:query')
+  .get(catchAsync(api.oneAccessory))
 
-router.route('/items/:query')
-  .get(catchAsync(api.oneItem))
-
+// Armor
 router.route('/armors')
   .get(catchAsync(api.allArmor))
-
+router.route('/armors/random')
+  .get(catchAsync(api.randomArmor))
 router.route('/armors/:query')
   .get(catchAsync(api.oneArmor))
 
+// Helmets
 router.route('/helmets')
   .get(catchAsync(api.allHelmets))
-
+router.route('/helmets/random')
+  .get(catchAsync(api.randomHelmet))
 router.route('/helmets/:query')
   .get(catchAsync(api.oneHelmet))
 
+// Items
+router.route('/items')
+  .get(catchAsync(api.allItems))
+router.route('/items/random')
+  .get(catchAsync(api.randomItem))
+router.route('/items/:query')
+  .get(catchAsync(api.oneItem))
+
+// Weapons
 router.route('/weapons')
   .get(catchAsync(api.allWeapons))
-
+router.route('/weapons/random')
+  .get(catchAsync(api.randomWeapon))
 router.route('/weapons/:query')
   .get(catchAsync(api.oneWeapon))
-
-router.route('/accessories')
-  .get(catchAsync(api.allAccessories))
-
-router.route('/accessories/:query')
-  .get(catchAsync(api.oneAccessory))
 
 module.exports = router;
